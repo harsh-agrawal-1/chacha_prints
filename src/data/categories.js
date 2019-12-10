8d6edbbfa4766488a91da2e5caacf8257f99aa4b
HarshPC <96harsh.agrawal@gmail.com>
2019-12-10 21:44:23 +0530

    imagekit links added

--- src/data/categories.js
+++ src/data/categories.js
@@ -2,63 +2,63 @@ export const categories = [
   {
     name: 'Gol Poshak',
     imgUrl:
-      'https://www.dropbox.com/s/ivi41ifk3tdqr4h/Poshak.png?raw=1',
+      'https://www.dropbox.com/s/uyrxbqdyikl7c8b/poshak_lowres.jpg?raw=1',
     desc: 'Poshak for laddu gopal',
     link: 'poshak',
   },
   {
     name: 'Pagadi',
     imgUrl:
-      'https://www.dropbox.com/s/y6h6lz02lyc18r3/Pagadi.png?raw=1',
+      'https://www.dropbox.com/s/ezzstuazcwq0vpz/pagadi_lowres.jpg?raw=1',
     desc: 'Pagadi for laddu gopal',
     link: 'pagadi',
   },
   {
     name: 'Mukut',
     imgUrl:
-      'https://www.dropbox.com/s/584r76m6g5hfy5k/Mukut.png?raw=1',
+      'https://www.dropbox.com/s/b8tnfflbkqjnccu/mukut_lowres.jpg?raw=1',
     desc: 'Mukut for laddu gopal',
     link: 'mukut',
   },
   {
     name: 'Mala',
     imgUrl:
-      'https://www.dropbox.com/s/v1ajkw60znw6xj3/Mala.png?raw=1',
+      'https://www.dropbox.com/s/ug167e86i9r6de3/mala_lowres.jpg?raw=1',
     desc: 'Mala for laddu gopal',
     link: 'mala',
   },
   {
     name: 'Durga Poshak',
     imgUrl:
-      'https://www.dropbox.com/s/ccnypw95xydmguc/Durga.png?raw=1',
+      'https://www.dropbox.com/s/iejijje2ku0oys4/durga_lowres.jpg?raw=1',
     desc: 'Poshak for durga maa',
     link: 'durga',
   },
   {
     name: 'Idols',
     imgUrl:
-      'https://www.dropbox.com/s/rgq64f7n3pw0jgu/Idol.png?raw=1',
+      'https://www.dropbox.com/s/r24g6z34xjdgou0/idol_lowres.jpg?raw=1',
     desc: 'Idols / Murtis',
     link: 'idols',
   },
   {
     name: 'Jhoola',
     imgUrl:
-      'https://www.dropbox.com/s/kvbhr1kk40v39vy/Jhoola.png?raw=1',
+      'https://www.dropbox.com/s/yfwbwxn7n8gjvyh/jhoola_lowres.jpg?raw=1',
     desc: 'Jhoola for laddu gopal',
     link: 'jhoola',
   },
   {
     name: 'Singhasan',
     imgUrl:
-      'https://www.dropbox.com/s/40z4uon7uhcr9fn/Singhasan.png?raw=1',
+      'https://www.dropbox.com/s/wxscofv5yt3xfbd/singhasan_lowres.jpg?raw=1',
     desc: 'Singhasan for laddu gopal',
     link: 'singhasan',
   },
   {
     name: 'Lehenga Patka',
     imgUrl:
-      'https://www.dropbox.com/s/ls3my5vuvrnv9q9/LP.png?raw=1',
+      'https://www.dropbox.com/s/6jrcr0ibfb7ylj3/lp_lowres.jpg?raw=1',
     desc: 'Lehenga Patka for all idols',
     link: 'lp',
   },
--- src/data/poshak.js
+++ src/data/poshak.js
@@ -2,29 +2,37 @@ export const poshak = [
   {
     name: 'Mor Poshak (Set of 6)',
     imgUrl:
-      'https://www.dropbox.com/s/c1f66fu903wavc3/00001-01.png?raw=1',
+      'https://ik.imagekit.io/xe9lmicvfek/Poshak/00001-01_TNw0cNW1K.png?tr=w-00,h-200',
     desc: 'Poshak for laddu gopal',
     cost: 1000,
   },
   {
     name: 'Mor Poshak (Set of 4)',
     imgUrl:
-      'https://www.dropbox.com/s/y7zdvhajsbbpruq/00002-01.png?raw=1',
+      'https://ik.imagekit.io/xe9lmicvfek/Poshak/00002-01_xeilx5-R2.png',
     desc: 'Poshak for laddu gopal',
     cost: 1000,
   },
   {
     name: 'Fancy Poshak (Set of 4)',
     imgUrl:
-      'https://www.dropbox.com/s/s9krt8nncfv5bna/00003-01.png?raw=1',
+      'https://ik.imagekit.io/xe9lmicvfek/Poshak/00003-01_2dg9cLBzyf.png',
     desc: 'Poshak for laddu gopal',
     cost: 1000,
   },
   {
     name: 'Flower Poshak (Set of 6)',
     imgUrl:
-      'https://www.dropbox.com/s/oorazwmzl0wp8pz/00004-01.png?raw=1',
+      'https://ik.imagekit.io/xe9lmicvfek/Poshak/00004-01_7RTMPe4GY.png',
+    desc: 'Poshak for laddu gopal',
+    cost: 1500,
+  },
+  {
+    name: 'Flower Poshak (Set of 6)',
+    imgUrl:
+      'https://ik.imagekit.io/xe9lmicvfek/Poshak/00005/5no_lowres_qqtiyLM33W.jpg?tr=w-280,h-200',
     desc: 'Poshak for laddu gopal',
     cost: 1500,
   },
 ]
+//?tr=w-640,h-480
